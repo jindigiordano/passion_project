0e0637ed5b1d8a5a3395bf23c5f5b987c6f36352
Jin Di Giordano <jin.di.giordano@gmail.com>
2016-12-22 20:42:03 -0800

    Add CSS styling, further fix routes

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -12,40 +12,14 @@ post '/characters' do
   species_matchobj = /\bspecies\b\/\d/.match(random_species.to_s)
   species_num = species_matchobj.to_s[-1].to_s
   species_obj = JSON.parse(Swapi.get_species species_num)
-  p species_matchobj
-  p species_num
-  p species_obj
-  p species_obj["name"]
+
 
   all_planets = JSON.parse(Swapi.get_all 'planets')
   random_planet = all_planets["results"][rand(0..all_species.length)]
   planet_matchobj = /\bplanets\b\/\d/.match(random_planet.to_s)
   planet_num = planet_matchobj.to_s[-1].to_s
   planet_obj = JSON.parse(Swapi.get_planet planet_num)
-  p planet_matchobj
-  p planet_num
-  p planet_obj
-  p planet_obj["name"]
 
-  # all_vehicles = JSON.parse(Swapi.get_all 'vehicles')
-  # random_vehicle = all_vehicles["results"].sample
-  # vehicle_matchobj = /\bvehicles\b\/\d/.match(random_vehicle.to_s)
-  # vehicle_num = vehicle_matchobj.to_s[-1].to_s
-  # vehicle_obj = JSON.parse(Swapi.get_vehicle vehicle_num)
-  # p vehicle_matchobj
-  # p vehicle_num
-  # p vehicle_obj
-  # p vehicle_obj["name"]
-
-  # all_starships = JSON.parse(Swapi.get_all 'starships')
-  # random_starship = all_starships["results"].sample
-  # starship_matchobj = /\bstarships\b\/\d/.match(random_starship.to_s)
-  # starship_num = starship_matchobj.to_s[-1].to_s
-  # starship_obj = JSON.parse(Swapi.get_starship starship_num)
-  # p starship_matchobj
-  # p starship_num
-  # p starship_obj
-  # p starship_obj["name"]
 
   professions = ['Aerobat', 'Antarian Ranger', 'Big Game Hunter', 'Bodyguard',
               'Bounty Hunter', 'Changeling', 'Chief Engineer', 'Crime Lord',
@@ -78,14 +52,14 @@ post '/characters' do
   if @character.save
     redirect "characters/#{@character.id}"
   else
-    erb :'characters/_new'
+    erb :'characters/new'
   end
 end
 
 # new
 get '/characters/new' do
   if current_user
-    erb :'characters/_new'
+    erb :'characters/new'
   else
     #TODO show user a helpful error message
     redirect 'characters'
@@ -98,7 +72,7 @@ get '/characters/:id' do
   @user = User.find(@character.user_id)
 
 
-  erb :'characters/_show'
+  erb :'characters/show'
 end
 
 # destroy
